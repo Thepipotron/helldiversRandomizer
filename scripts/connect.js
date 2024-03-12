370ece9f5fd585d14635d52829fefa358ace291c
thepipotron <payton.fack@okstate.edu>
2024-03-12 21:13:51 +0100

    Add remaining images and table definitions

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,42 +1,76 @@
-const body = document.body,
-tbl = new Tabulator("#weapontable", {
-   data:PrimaryWeapons,
-   selectableRows:true,
-   columns:[
-	   {title:"Name",field:"name",hozAlign:"left",vertAlign:"middle"},
-	   {title:"Image",field:"image", formatter:"image",minWidth : 200, formatterParams:{
-	   }},
-   ],
+const weaponTable = new Tabulator("#weapontable", {
+  dataTree: true,
+  data: WeaponList,
+  selectableRows: true,
+  dataTreeElementColumn: "name",
+  dataTreeChildIndent: 0,
+  dataTreeSelectPropagate: true,
+  layout: "fitDataTable",
+  headerVisible: false,
+  columns: [
+    {
+      // Weapon group
+      title: "Weapons",
+      columns: [
+        {
+          title: "",
+          field: "name",
+          hozAlign: "left",
+          vertAlign: "middle",
+          minWidth: 250,
+        },
+        {
+          title: "",
+          field: "image",
+          formatter: "image",
+          minWidth: 200,
+          maxWidth: 400,
+        },
+      ],
+    },
+  ],
 });
-tbl2 = new Tabulator("#weapontable2", {
-	data:PrimaryWeapons,
-	selectableRows:true,
-	columns:[
-		{title:"Name",field:"name",hozAlign:"left",vertAlign:"middle"},
-		{title:"Image",field:"image", formatter:"image",minWidth : 200, formatterParams:{
-		}},
-	],
- });
-function Startup(){
 
-	// Display weapon list
+const stratagemTable = new Tabulator("#stratagemtable", {
+  dataTree: true,
+  data: StratagemList,
+  selectableRows: true,
+  dataTreeElementColumn: "name",
+  dataTreeChildIndent: 0,
+  dataTreeSelectPropagate: true,
+  layout: "fitDataTable",
+  headerVisible: false,
+  columns: [
+    {
+      // Weapon group
+      title: "Strategem",
+      columns: [
+        {
+          title: "",
+          field: "name",
+          hozAlign: "left",
+          vertAlign: "middle",
+          minWidth: 250,
+        },
+        {
+          title: "",
+          field: "image",
+          formatter: function (cell, formatterParams) {
+            if (cell.getValue() == "") {
+              return "";
+            }
 
+            var img = document.createElement("object");
+            img.data = cell.getValue();
+            img.width = 100;
+            img.height = 100;
 
-/*
-	for(var i = 0; i < PrimaryWeapons.length; i++){
-		console.log("Appending " + i);
-		const tr = tbl.insertRow();
-		const weaponName = tr.insertCell();
-		const weaponImage = tr.insertCell();
-		let weapon = PrimaryWeapons[i];
-
-		weaponName.appendChild(document.createTextNode(weapon[0]));
-		weaponName.style.border = '1px solid black';
-
-
-		var image = document.createElement('img');
-		image.src = weapon[1];
-		weaponImage.appendChild(image);
-	}
-*/
-}
+            return img;
+          },
+          minWidth: 200,
+          maxWidth: 400,
+        },
+      ],
+    },
+  ],
+});
--- scripts/items.js
+++ scripts/items.js
@@ -1,23 +1,219 @@
 // list definitions for entries are as follows for each entry
 // Name, image
-var PrimaryWeapons = [
-{name:"AR23-Liberator", image:"images/Primary/AR23-Liberator.webp"},
-{name:"MP-98 Knight", image:"images/Primary/MP-98 Knight.webp"},
-{name:"R-63 Diligence",image:"images/Primary/R-63 Diligence.webp"},
-{name:"SMG-37 Defender", image:"images/Primary/SMG-37 Defender.webp"},
-{name:"SG-8 Punisher", image:"images/Primary/SG-8 Punisher.webp"},
-{name:"SG-225 Breaker", image:"images/Primary/SG-225 Breaker.webp"},
-{name:"SG-225IE Breaker", image:"images/Primary/SG-2251E Breaker Incendiary.webp"},
-{name:"LAS-5 Scythe", image:"images/Primary/LAS-5 Scythe.webp"},
-{name:"PLAS-1 Scorcher", image:"images/Primary/PLAS-1 Scorcher.webp"},
-{name:"AR23-P Liberator Penetrator", image:"images/Primary/AR23-P Liberator Penetrator.webp"},
-{name:"AR23-E Liberator Explosive", image:"images/Primary/AR23-E Liberator Explosive.webp"},
-{name:"R63-CS Diligence Counter Sniper",  image:"images/Primary/R63-CS Diligence Counter Sniper.webp"},
-{name:"SG-8S Slugger",  image:"images/Primary/SG-8S Slugger.webp"},
-{name:"SG-225SP Breaker Spray & Pray",  image:"images/Primary/SG-225SP Breaker Spray & Pray.webp"},
-{name:"JAR-5 Dominator",  image:"images/Primary/JAR-5 Dominator.webp"},
-]
+var WeaponList = [
+  {
+    name: "Primary",
+    image: "",
+    _children: [
+      { name: "AR23-Liberator", image: "images/Primary/AR23-Liberator.webp" },
+      { name: "MP-98 Knight", image: "images/Primary/MP-98 Knight.webp" },
+      { name: "R-63 Diligence", image: "images/Primary/R-63 Diligence.webp" },
+      { name: "SMG-37 Defender", image: "images/Primary/SMG-37 Defender.webp" },
+      { name: "SG-8 Punisher", image: "images/Primary/SG-8 Punisher.webp" },
+      { name: "SG-225 Breaker", image: "images/Primary/SG-225 Breaker.webp" },
+      {
+        name: "SG-225IE Breaker",
+        image: "images/Primary/SG-2251E Breaker Incendiary.webp",
+      },
+      { name: "LAS-5 Scythe", image: "images/Primary/LAS-5 Scythe.webp" },
+      { name: "PLAS-1 Scorcher", image: "images/Primary/PLAS-1 Scorcher.webp" },
+      {
+        name: "AR23-P Liberator Penetrator",
+        image: "images/Primary/AR23-P Liberator Penetrator.webp",
+      },
+      {
+        name: "AR23-E Liberator Explosive",
+        image: "images/Primary/AR23-E Liberator Explosive.webp",
+      },
+      {
+        name: "R63-CS Diligence Counter Sniper",
+        image: "images/Primary/R63-CS Diligence Counter Sniper.webp",
+      },
+      { name: "SG-8S Slugger", image: "images/Primary/SG-8S Slugger.webp" },
+      {
+        name: "SG-225SP Breaker Spray & Pray",
+        image: "images/Primary/SG-225SP Breaker Spray & Pray.webp",
+      },
+      { name: "JAR-5 Dominator", image: "images/Primary/JAR-5 Dominator.webp" },
+    ],
+  },
 
-// list definitions for entries are as follows for each entry
-// Name, image
-let SecondaryWeapons = []
+  {
+    name: "Secondary",
+    image: "",
+    _children: [
+      { name: "P-19 Redeemer", image: "images/Secondary/P-19 Redeemer.webp" },
+      { name: "P-2 Peacemaker", image: "images/Secondary/P-2 Peacemaker.webp" },
+      { name: "P-4 Senator", image: "images/Secondary/P-4 Senator.webp" },
+    ],
+  },
+];
+
+var StratagemList = [
+  {
+    name: "Patriotic Administration Center ",
+    image: "",
+    _children: [
+      { name: "Machine Gun", image: "images/PAC/Machine Gun.svg" },
+      {
+        name: "Anti-Material Rifle",
+        image: "images/PAC/Anti-Materiel Rifle.svg",
+      },
+      { name: "Stalwart", image: "images/PAC/Stalwart.svg" },
+      {
+        name: "Expendable Anti-Tank",
+        image: "images/PAC/Expendable Anti-Tank.svg",
+      },
+      { name: "Recoilless Rifle", image: "images/PAC/Recoilless Rifle.svg" },
+      { name: "Flamethrower", image: "images/PAC/Flamethrower.svg" },
+      { name: "Autocannon", image: "images/PAC/Autocannon.svg" },
+      { name: "Railgun", image: "images/PAC/Railgun.svg" },
+      { name: "Spear", image: "images/PAC/Spear.svg" },
+    ],
+  },
+  {
+    name: "Orbital Cannons",
+    image: "",
+    _children: [
+      {
+        name: "Orbital 120MM HE Barrage",
+        image: "images/Orbital/Orbital 120MM HE Barrage.svg",
+      },
+      { name: "Orbital Laser", image: "images/Orbital/Orbital Laser.svg" },
+      {
+        name: "Orbital 380MM HE Barrage",
+        image: "images/Orbital/Orbital 380MM HE Barrage.svg",
+      },
+      {
+        name: "Orbital Railcannon Strike",
+        image: "images/Orbital/Orbital Railcannon Strike.svg",
+      },
+      {
+        name: "Orbital Airburst Strike",
+        image: "images/Orbital/Orbital Airburst Strike.svg",
+      },
+      {
+        name: "Orbital Walking Barrage",
+        image: "images/Orbital/Orbital Walking Barrage.svg",
+      },
+      {
+        name: "Orbital Gatling Barrage",
+        image: "images/Orbital/Orbital Gatling Barrage.svg",
+      },
+    ],
+  },
+  {
+    name: "Hanger",
+    image: "",
+    _children: [
+      {
+        name: "Eagle 110MM Rocket Pods",
+        image: "images/Hanger/Eagle 110MM Rocket Pods.svg",
+      },
+      { name: "Eagle 500KG Bomb", image: "images/Hanger/Eagle 500KG Bomb.svg" },
+      { name: "Eagle Airstrike", image: "images/Hanger/Eagle Airstrike.svg" },
+      {
+        name: "Eagle Cluster Bomb",
+        image: "images/Hanger/Eagle Cluster Bomb.svg",
+      },
+      {
+        name: "Eagle Napalm Airstrike",
+        image: "images/Hanger/Eagle Napalm Airstrike.svg",
+      },
+      { name: "Eagle Rearm", image: "images/Hanger/Eagle Rearm.svg" },
+      {
+        name: "Eagle Smoke Strike",
+        image: "images/Hanger/Eagle Smoke Strike.svg",
+      },
+      {
+        name: "Eagle Strafing Run",
+        image: "images/Hanger/Eagle Strafing Run.svg",
+      },
+      { name: "Jump Pack", image: "images/Hanger/Jump Pack.svg" },
+    ],
+  },
+  {
+    name: "Bridge",
+    image: "",
+    _children: [
+      { name: "HMG Emplacement", image: "images/Bridge/HMG Emplacement.svg" },
+      {
+        name: "Orbital EMS Strike",
+        image: "images/Bridge/Orbital EMS Strike.svg",
+      },
+      {
+        name: "Orbital Gas Strike",
+        image: "images/Bridge/Orbital Gas Strike.svg",
+      },
+      {
+        name: "Orbital Precision Strike",
+        image: "images/Bridge/Orbital Precision Strike.svg",
+      },
+      {
+        name: "Orbital Smoke Strike",
+        image: "images/Bridge/Orbital Smoke Strike.svg",
+      },
+      {
+        name: "Shield Generator Relay",
+        image: "images/Bridge/Shield Generator Relay.svg",
+      },
+      { name: "Tesla Tower", image: "images/Bridge/Tesla Tower.svg" },
+    ],
+  },
+  {
+    name: "Engineering Bay",
+    image: "",
+    _children: [
+      {
+        name: "Anti-Personnel Minefield",
+        image: "images/Engineering/Anti-Personnel Minefield.svg",
+      },
+      { name: "Arc Thrower", image: "images/Engineering/Arc Thrower.svg" },
+      {
+        name: "Ballistic Shield Backpack",
+        image: "images/Engineering/Ballistic Shield Backpack.svg",
+      },
+      {
+        name: "Grenade Launcher",
+        image: "images/Engineering/Grenade Launcher.svg",
+      },
+      {
+        name: "Guard Dog Rover",
+        image: "images/Engineering/Guard Dog Rover.svg",
+      },
+      {
+        name: "Incendiary Mines",
+        image: "images/Engineering/Incendiary Mines.svg",
+      },
+      { name: "Laser Cannon", image: "images/Engineering/Laser Cannon.svg" },
+      {
+        name: "Shield Generator Pack",
+        image: "images/Engineering/Shield Generator Pack.svg",
+      },
+      { name: "Supply Pack", image: "images/Engineering/Supply Pack.svg" },
+    ],
+  },
+  {
+    name: "Robotics Workshop",
+    image: "",
+    _children: [
+      {
+        name: "Autocannon Sentry",
+        image: "images/Robotics/Autocannon Sentry.svg",
+      },
+      {
+        name: "EMS Mortar Sentry",
+        image: "images/Robotics/EMS Mortar Sentry.svg",
+      },
+      { name: "Gatling Sentry", image: "images/Robotics/Gatling Sentry.svg" },
+      { name: "Guard Dog", image: "images/Robotics/Guard Dog.svg" },
+      {
+        name: "Machine Gun Sentry",
+        image: "images/Robotics/Machine Gun Sentry.svg",
+      },
+      { name: "Mortar Sentry", image: "images/Robotics/Mortar Sentry.svg" },
+      { name: "Patriot Exosuit", image: "images/Robotics/Patriot Exosuit.svg" },
+      { name: "Rocket Sentry", image: "images/Robotics/Rocket Sentry.svg" },
+    ],
+  },
+];
